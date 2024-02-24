dae2b3029cbec4802ec146db961b99ee90cb45a5
vladkun2006 <113937249+vladkun2006@users.noreply.github.com>
2024-02-24 18:43:03 +0200

    fix mistakes

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,6 @@
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
-  const mobileheroRef = document.querySelector("[data-hero]");
 
   menuBtnRef.addEventListener("click", () => {
     const expanded =
@@ -11,7 +10,5 @@
     menuBtnRef.setAttribute("aria-expanded", !expanded);
 
     mobileMenuRef.classList.toggle("is-open");
-
-    mobileheroRef.classList.toggle("is-open");
   });
 })();
